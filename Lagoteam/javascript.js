7b55ae2e0850314208f2ef85869378589f67cce5
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2018-10-26 11:24:27 +0300

    first commit

---
+++ Lagoteam/javascript.js
@@ -0,0 +1,17 @@
+function clickme1(q){
+	var q = document.getElementById('visible1');
+	q.style.visibility="visible";	
+}
+function clickme2(w){
+	var w = document.getElementById('visible2');
+	w.style.visibility="visible";
+}
+
+function blur1(a){
+	var a = document.getElementById('visible1');
+	a.style.visibility="hidden";	
+}
+function blur2(s){
+	var s = document.getElementById('visible2');
+	s.style.visibility="hidden";
+}
