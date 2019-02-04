0d01888f463f501b32323f1b40b464bf889930ee
Dmitriy Kaminskiy <dima.vampire98@gmail.com>
2019-02-04 17:39:56 +0200

    commit

---
+++ cackes/js/script.js
@@ -0,0 +1,27 @@
+$('.feedback').slick({
+				    autoplay: true,
+				    autoplaySpeed: 2000,
+				    speed: 1500,
+				    cssEase: 'ease-in-out',
+				    centerMode: true,
+				    slidesToShow: 1,
+				    centerPadding: '0px',
+				    dots: true,
+				    pauseOnDotsHover: true,
+				    lazyLoad: 'progressive',
+				    arrows: false
+				});
+
+	      $('.banner_block').slick({
+				    autoplay: true,
+				    autoplaySpeed: 2000,
+				    speed: 1500,
+				    cssEase: 'ease-in-out',
+				    centerMode: true,
+				    slidesToShow: 1,
+				    centerPadding: '20px',
+				    dots: false,
+				    pauseOnDotsHover: true,
+				    lazyLoad: 'progressive',
+				    arrows: false
+				});
